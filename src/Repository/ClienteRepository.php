cf5356833e7e1d40eb99d32e2411f0f7095e468b
Kristian Frey <kristianfrey11@gmail.com>
2026-01-15 12:48:29 -0300

    feat: impelmentado método de alterar e deletar cliente

--- src/Repository/ClienteRepository.php
+++ src/Repository/ClienteRepository.php
@@ -3,10 +3,11 @@
 interface ClienteRepository
 {
     public function criarCliente($nome, $telefone, $email);
-    public function deletarCliente(Cliente $cliente);
-    public function alterarCliente(Cliente $cliente);
-    public function salvarCliente(Cliente $cliente);
+    public function deletarCliente($id);
+    public function alterarCliente($id, $nome, $telefone, $email);
+    public function salvarCliente($id, $nome, $telefone, $email);
     public function buscaClientePorNome(Cliente $cliente);
+    public function buscaClientePorId($id);
     public function buscaTodosClientes();
     public function verificaEmail($email);
 }
